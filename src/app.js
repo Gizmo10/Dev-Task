a7371fdcbe6cee7e757c145d6f35476221c12593
Sibusiso Wanda Ndzala <sw.ndzala@gmail.com>
2025-06-16 17:14:53 +0200

    Added home page

--- src/app.js
+++ src/app.js
@@ -1,8 +1,14 @@
 const express = require("express");
+const path = require("path");
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+app.use(express.static(path.join(__dirname, "src")));
 app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "index.html"));
+});
 
 app.post("/convertString", (req, res) => {
   try {
--- src/convert_string.js
+++ src/convert_string.js
@@ -1,26 +1,41 @@
+const urlElement = document.getElementById("url");
+const emailElement = document.getElementById("email");
+const resultElement = document.getElementById("result");
+const submitButton = document.getElementById("submit");
+
+submitButton.onclick = async () => {
+  const urlValue = urlElement.value;
+  const emailValue = emailElement.value;
+  console.log(urlValue);
+  console.log(emailValue);
+};
+
 const convertString = async function (data) {
   try {
-    const response = await fetch(`http://localhost:3001/convertString`, {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(data),
-    });
+    const response = await fetch(
+      `https://dev-task-79hh.onrender.com/convertString`,
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(data),
+      }
+    );
 
     if (!response.ok) {
       throw new Error(`HTTP Error: ${response.status}`);
     }
 
     const result = await response.json();
-    //console.log(result);
-    return result;
+    console.log(result);
+    //return result;
   } catch (error) {
     throw new Error(`Failed to convert: ${error.message}`);
   }
 };
-const obj = {
+/*const obj = {
   data: "String",
-};
+};*/
 
-convertString(obj);
+//convertString(obj);
