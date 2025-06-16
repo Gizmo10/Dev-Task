715ec897ca029295032c895e99dfff321f940d13
Sibusiso Wanda Ndzala <sw.ndzala@gmail.com>
2025-06-16 20:54:04 +0200

    Installed cors

--- src/app.js
+++ src/app.js
@@ -1,10 +1,12 @@
 const express = require("express");
+const cors = require("cors");
 const path = require("path");
 const app = express();
 const PORT = process.env.PORT || 3001;
 
 app.use(express.static(path.join(__dirname, "src")));
 app.use(express.json());
+app.use(cors());
 
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "index.html"));
